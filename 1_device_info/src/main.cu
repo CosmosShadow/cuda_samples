8bf842c08ead0976eece950a85269a61e9dcc913
lichen <lichenarthurdata@gmail.com>
2017-09-15 01:58:44 +0800

    3_square

--- 1_device_info/src/main.cu
+++ 1_device_info/src/main.cu
@@ -2,6 +2,25 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
+void printDeviceProp(const cudaDeviceProp &prop)
+{
+    printf("Device Name : %s.\n", prop.name);
+    printf("totalGlobalMem : %lu.\n", prop.totalGlobalMem);
+    printf("sharedMemPerBlock : %lu.\n", prop.sharedMemPerBlock);
+    printf("regsPerBlock : %d.\n", prop.regsPerBlock);
+    printf("warpSize : %d.\n", prop.warpSize);
+    printf("memPitch : %lu.\n", prop.memPitch);
+    printf("maxThreadsPerBlock : %d.\n", prop.maxThreadsPerBlock);
+    printf("maxThreadsDim[0 - 2] : %d %d %d.\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+    printf("maxGridSize[0 - 2] : %d %d %d.\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+    printf("totalConstMem : %lu.\n", prop.totalConstMem);
+    printf("major.minor : %d.%d.\n", prop.major, prop.minor);
+    printf("clockRate : %d.\n", prop.clockRate);
+    printf("textureAlignment : %lu.\n", prop.textureAlignment);
+    printf("deviceOverlap : %d.\n", prop.deviceOverlap);
+    printf("multiProcessorCount : %d.\n", prop.multiProcessorCount);
+}
+
 bool InitCUDA()
 {
     int count;
@@ -16,7 +35,7 @@ bool InitCUDA()
     for (i = 0; i < count; i++) {
         cudaDeviceProp prop;
         if (cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
-            std::cout << "device " << i << " version: " << prop.major << "." << prop.minor << std::endl;
+            printDeviceProp(prop);
             if (prop.major >= 1) {
                 index = i;
             }
